9b7d04bb1066a8f3f5ba9f3bbfded36a24307113
vihnguyen <nguye29v@mtholyoke.edu>
2018-02-16 01:37:54 -0500

    Add button icons

--- app/app.js
+++ app/app.js
@@ -20,9 +20,3 @@ function scrollFunction() {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
