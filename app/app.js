0e911d6a84920efc09f9931d9006aad6200d2e6f
vihnguyen <nguye29v@mtholyoke.edu>
2018-02-15 21:28:06 -0500

    Fix back to top button, modify navbar to black

--- app/app.js
+++ app/app.js
@@ -23,6 +23,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
