948b26271323cf20ef9de0072a6283cc94e8e797
vihnguyen <nguye29v@mtholyoke.edu>
2018-03-08 01:04:55 -0500

    Add admin.py

--- lessthanthree/e_Vent/models.py
+++ lessthanthree/e_Vent/models.py
@@ -31,7 +31,7 @@ class Price(models.Model):
     """
     Model representing an event price (e.g. $0, $5.5).
     """
-    name = models.FloatField(initial='0', help_text="Enter your event price (e.g. 0, 5.5, 10).")
+    name = models.FloatField(help_text="Enter your event price (e.g. 0, 5.5, 10).")
 
     def __str__(self):
         """
@@ -44,7 +44,7 @@ class Popularity(models.Model):
     """
     Model representing an event popularity
     """
-    name = models.PositiveIntegerField(required=True, initial='0', help_text="Popularity of the event")
+    name = models.PositiveIntegerField(help_text="Popularity of the event")
 
     def __str__(self):
         """
@@ -56,31 +56,30 @@ class Event(models.Model):
     """
     Model representing an event
     """
-    title = models.CharField(required=True, max_length=100)
+    title = models.CharField(max_length=100)
 
     user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     # Foreign Key used because events can only have one user, but users can have multiple events
     # Users as a string rather than object because it hasn't been declared yet in the file.
 
-    tag = models.ManyToManyField(Tag, required=True, help_text="Select a tag for this book")
+    tag = models.ManyToManyField(Tag, help_text="Select a tag for this book")
     # ManyToManyField used because a tag can contain many events. Events can cover many tags.
 
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
 
-    description = models.TextField(max_length=1000, required=True, help_text="Enter a brief description of the event")
+    description = models.TextField(max_length=1000, help_text="Enter a brief description of the event")
 
-    start_time = models.DateTimeField(required=True, auto_now_add=True, help_text="Enter the starting date and time of your event");
-    end_time = models.DateTimeField(required=True, auto_now_add=True, help_text="Enter the ending date and time of your event");
+    start_time = models.DateTimeField(auto_now_add=True, help_text="Enter the starting date and time of your event");
+    end_time = models.DateTimeField(auto_now_add=True, help_text="Enter the ending date and time of your event");
 
-    price = models.ForeignKey(Price, required= True, on_delete=models.SET_NULL, null=True)
+    price = models.ForeignKey(Price, on_delete=models.SET_NULL, null=True)
 
     #TODO 2: Needs to modify
     picture = models.ImageField(upload_to = 'imgs/', default = 'imgs/None/no-img.jpg')
-
-    attendees = models.ManyToManyField('User', on_delete=models.SET_NULL, null=True)
+    #attendees = models.ManyToManyField('User')
 
     #TODO 3: Needs to modify to be PositiveIntegerField with the value is the number of attendees
-    popularity = models.ForeignKey(Popularity, required= True, on_delete=models.SET_NULL, null=True)
+    popularity = models.ForeignKey(Popularity, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """
@@ -111,9 +110,9 @@ class User(models.Model):
     #TODO 4: Needs to modify
     picture = models.ImageField(upload_to = 'imgs/', default = 'imgs/None/no-img.jpg')
 
-    contact = models.CharField(required=True, max_length=100, help_text="Enter your email address or phone number.")
+    contact = models.CharField(max_length=100, help_text="Enter your email address or phone number.")
 
-    bio = models.TextField(max_length=1000, required=True, help_text="Enter a brief description of you/your organization.")
+    bio = models.TextField(max_length=1000, help_text="Enter a brief description of you/your organization.")
 
 
     def get_absolute_url(self):
