65c7da473f7459a6e7cd04b309a5a32b981a729c
camillepham <pham29a@mtholyoke.edu>
2018-03-26 01:31:26 -0400

    Fix views.py so that templates could show up as html

--- lessthanthree/e_Vent/views.py
+++ lessthanthree/e_Vent/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views import generic
 from .models import Location, Tag, Price, Popularity, Event, User
+from django.views import generic
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context = {},
+    )
 
 class IndexView(generic.ListView):
     model = Event
+
