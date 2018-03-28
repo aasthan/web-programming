ea0515ede37baa39028e266cba2e6a248bac2bf7
aasthan <nirau22a@mtholyoke.edu>
2018-03-28 02:03:11 -0400

    Change views.py to show filtered query result

--- lessthanthree/e_Vent/views.py
+++ lessthanthree/e_Vent/views.py
@@ -4,6 +4,25 @@ from django.shortcuts import render
 from .models import Location, Tag, Price, Popularity, Event, User
 from django.views import generic
 
-class IndexView(generic.ListView):
+def indexView(request):
+	"""
+	View function for the home page
+	"""
+	#Generate counts of some of the main objects
+	num_events = Event.objects.all().count()
+	events = Event.objects.all()
+	num_users = User.objects.all().count()
+	num_location = Location.objects.all().count()
+	popular_events = Event.objects.filter(popularity__gt = 10)
+
+	return render(
+		request,
+		'index.html',
+		context={'events':events,'num_events':num_events,'num_users':num_users,'num_location':num_location,'popular_events':popular_events},
+
+		)
+
+"""class IndexView(generic.ListView):
     model = Event
-    template_name = 'index.html'
+    template_name = 'index2.html'"""
+    
