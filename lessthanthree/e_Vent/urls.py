db6f7de95b6c7a0de536391b338cd82904ccb8a8
vihnguyen <nguye29v@mtholyoke.edu>
2018-03-29 17:16:15 -0400

    Fix search function

--- lessthanthree/e_Vent/urls.py
+++ lessthanthree/e_Vent/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('search/', views.EventSearchView.as_view(), name='search'),
     path('', views.indexView, name='index'),
     path('event/<int:pk>', views.EventDetailView.as_view(), name='event-details'),
-    path('search/', views.EventSearchView.as_view(), name='search-results'),
     path('eventForm/', views.eventForm, name='event-form'),
     path('profile/',views.YourEventsView.as_view(), name='profile'),
     path('form/',views.EventFormView.as_view(),name='form'),
